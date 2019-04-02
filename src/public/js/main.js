0cc72144494e42b7b1bf82ec0e87b977c8f07322
prmorais13 <prmorais_13@hotmail.com>
2019-04-02 16:43:51 -0300

    Adicionando funcionalidade no form de usuario

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,11 +6,37 @@ $(function() {
   const $messageBox = $('#message');
   const $chat = $('#chat');
 
+  // Obtendo os elementos do DOM do form nickname
+  const nickForm = $('#nickForm');
+  const nickName = $('#nickName');
+  const nickError = $('#nickError');
+
+  const $users = $('#usernames');
+
   // Eventos
   $messageForm.submit(e => {
     e.preventDefault();
     socket.emit('send message', $messageBox.val());
     $messageBox.val('');
+  });
+
+  nickForm.submit(e => {
+    e.preventDefault();
+    socket.emit('new user', nickName.val(), data => {
+      if (data) {
+        $('#nickWrap').hide();
+        $('#contentWrap').show();
+      } else {
+        nickError.html(`
+          <div class="alert alert-danger">
+            Este usuário já existe!
+          </div>
+        
+        `);
+      }
+
+      nickName.val('');
+    });
   });
 
   socket.on('new message', dados => {
--- src/sockets.js
+++ src/sockets.js
@@ -1,6 +1,18 @@
 module.exports = io => {
+  let nicknames = [];
+
   io.on('connection', socket => {
-    console.log('Um usuário conectado.');
+    // console.log('Um usuário conectado.');
+
+    socket.on('new user', (data, cb) => {
+      if (nicknames.indexOf(data) != -1) {
+        cb(false);
+      } else {
+        cb(true);
+        socket.nickname = data;
+        nicknames.push(socket.nickname);
+      }
+    });
 
     socket.on('send message', dados => {
       io.sockets.emit('new message', dados);
